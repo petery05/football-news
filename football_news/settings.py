00ef39dcc4fd3622e3d920450815f8863bcf7ae1
HODEMASTER <peteryap0505@gmail.com>
2025-09-10 14:25:25 +0700

    tutorial 2 finished

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,6 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "peter-yap-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://peter-yap-footballnews.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
